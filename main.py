d19dd4f9604e016fe6ccb35827d897f72222066a
Kristofer Martin Pillarina <41129289+CodexApple@users.noreply.github.com>
2023-10-25 23:03:43 +0800

    Modified platform states
    Added support for shutting down Linux platforms using uname, or grep return, as well as MacOS.
    Modified windows checking from platform to platform.system() and sys.platform
    Fixed file formatting

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from datetime import datetime, time
 from tkinter import *
 from time import sleep
 
+
 DIALOG_DURATION_MS = 300000
 REFRESH_PERIOD_SECONDS = 60
 HOURLY_BREAK_IN_MINUTES = [0, 30]
@@ -16,8 +17,12 @@ class DialogueBox():
     def check_for_closing(self, event=Tk.event_info):
         if(event.char == CLOSE_KEY):
             self.win.destroy()
+
+
     def disable_closing():
         pass
+
+
     def __init__(self,message=str) -> None:
         self.win = Tk()
         self.win.geometry("600x350")
@@ -36,28 +41,46 @@ class DialogueBox():
         self.win.mainloop()
         pass
 
+
 def is_time_for_rest(current_time=datetime):
     if current_time.time() > FORCED_SLEEP_PERIOD[0] or current_time.time() < FORCED_SLEEP_PERIOD[1]:
         return True
     return False
+
 
 def is_time_for_break(current_time=datetime):
     if current_time.minute in HOURLY_BREAK_IN_MINUTES:
         return True
     return False
 
+
+def get_os_info():
+    return platform.system().lower()
+
+
 def force_shut_down():
-    if platform == "linux" or platform == "linux2":
-    # linux
-        return
-    elif platform == "darwin":
-    # OS X
-        return
-    elif platform == "win32":
+    # Missing guard checks for user with root access.
+
+    # Linux platform
+    if get_os_info() == "linux":
+        os.system("shutdown -h now")
+
+    # MacOS platform
+    elif get_os_info == "darwin":
+        os.system("shutdown -h now")
+
+    # Windows platform
+    # Modified windows checking platform, should use sys.platform to get "win32" and platform.system() for "windows"
+    elif get_os_info == "windows" or sys.platform == "win32":
         os.system("shutdown /s /t 1")
+
     DialogueBox(message="Computer Will Be shutting down.")
+
+
 def open_warning_dialog():
     DialogueBox(message="Hello! Please Take some rest first")
+
+
 def main():
     while(True):
         current_time = datetime.now()
